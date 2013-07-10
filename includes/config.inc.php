eab07933f7bde9fc69d8ce4d4fa8914f11b2f771
guesslara <glara.styleweb@gmail.com>
2013-07-10 08:52:36 -0500

    Primer Commit

---
+++ includes/config.inc.php
@@ -0,0 +1,16 @@
+<?
+	//configuracion de usuario MYSQL para primer base de datos
+	$host="localhost";
+	$usuario="root";
+	$pass="m4tr1x";
+	//tabla usuarios	
+	//base de datos
+	$db="2013_iqe_rac";
+	
+	/***********/
+	$strP="e10adc3949ba59abbe56e057f20f883e";
+	$tabla_usuarios="usuariosControl";
+	/**********/
+	/*directorio donde se encuentra la imagen*/
+	$dirImgPath="../../../../adm/rrhh2/fotos2/";
+?>
